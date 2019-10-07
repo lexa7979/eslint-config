f19f0648da7be1096802a4c1621d512ec021abfb
Alexander Urban <lexa@toxa.de>
2019-10-07 10:24:55 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# @lexa7979/eslint-config
+
+This package is feeded with my preferred settings for linting javascript code with ESLint.
+
+To use it, I include the following within `.eslintrc.js`
+
+``` js
+{
+    "extends": "@lexa7979"
+}
+```
---
+++ index.js
@@ -0,0 +1,134 @@
+module.exports = {
+	// https://eslint.org/docs/user-guide/configuring#specifying-environments
+    "env": {
+        "browser": true,
+		"es6": true,
+		"node": true,
+		// "jest/globals": true,
+	},
+	// https://eslint.org/docs/user-guide/configuring#extending-configuration-files
+	// "extends": "eslint:recommended",
+	"extends": [
+		"eslint:all",
+		// "plugin:react/recommended",
+	],
+	// https://eslint.org/docs/user-guide/configuring#specifying-globals
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+	},
+	// https://eslint.org/docs/user-guide/configuring#specifying-parser-options
+    "parserOptions": {
+        // "ecmaFeatures": {
+        //     "jsx": true
+        // },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+	},
+	// https://eslint.org/docs/user-guide/configuring#configuring-plugins
+    plugins: [
+		// "react",
+		// "jest",
+	],
+	// https://eslint.org/docs/user-guide/configuring#configuring-rules
+    rules: {
+		"object-curly-spacing": [ "error", "always" ],
+		"space-in-parens": [ "error", "always" ],
+		"quote-props": [ "error", "as-needed" ],
+		indent: [ "error", "tab" ],
+		"no-tabs": "off",
+		"sort-imports": "off",
+		"sort-keys": "off",
+		"space-before-function-paren": [ "error", {
+			anonymous: "always",
+			named: "never",
+			asyncArrow: "always"
+		} ],
+		"padded-blocks": [ "error", "never" ],
+		"arrow-parens": [ "error", "as-needed" ],
+		"max-len": [ "error", {
+			code: 120,
+			tabWidth: 4,
+			ignoreComments: true,
+		} ],
+		"no-ternary": "off",
+		"no-magic-numbers": "off",
+		"array-element-newline": [ "error", "consistent" ],
+		"object-property-newline": [ "error", {
+			allowAllPropertiesOnSameLine: true,
+		} ],
+		"comma-dangle": [ "error", "always-multiline" ],
+		"array-bracket-spacing": [ "error", "always" ],
+		"prefer-destructuring": [
+			"error",
+			{
+				VariableDeclarator: { array: false, object: true },
+				AssignmentExpression: { array: false, object: true }
+			},
+			{ enforceForRenamedProperties: false }
+		],
+		"one-var": [ "error", {
+			initialized: "never",
+			uninitialized: "always"
+		} ],
+		"key-spacing": [ "error", {
+			beforeColon: false,
+			afterColon: true,
+			mode: "minimum",
+			align: "value",
+		} ],
+		"multiline-ternary": [ "error", "always-multiline" ],
+		"multiline-comment-style": "off",
+		"capitalized-comments": "off",
+		"require-unicode-regexp": "off",
+		"wrap-regex": "off",
+		"require-jsdoc": [ "error", {
+			require: {
+				FunctionDeclaration: true,
+				MethodDefinition: true,
+				ClassDeclaration: true,
+				ArrowFunctionExpression: true,
+				FunctionExpression: true
+			}
+		} ],
+		"max-statements": [ "warn", 20 ],
+		"max-lines-per-function": [ "warn", 50 ],
+		"func-style": [ "error", "declaration", { allowArrowFunctions: true } ],
+		"no-multi-spaces": [ "error", {
+			ignoreEOLComments: true,
+			exceptions: {
+				Property: true,
+				VariableDeclarator: true,
+				ImportDeclaration: true,
+			}
+		} ],
+		"function-call-argument-newline": [ "error", "consistent" ],
+		"no-inline-comments": "off",
+		"line-comment-position": "off",
+		"operator-linebreak": [ "error", "before" ],
+		"brace-style": [ "error", "1tbs", { allowSingleLine: true } ],
+		"no-mixed-operators": [ "error" ],
+		"no-extra-parens": [ "error", "all", {
+			nestedBinaryExpressions: false,
+			enforceForArrowConditionals: false,
+		} ],
+		"lines-around-comment": [ "error", {
+			beforeLineComment: false,
+			beforeBlockComment: true,
+			afterLineComment: false,
+			allowClassStart: true,
+			allowObjectStart: true,
+		} ],
+		"no-plusplus": "off",
+		"dot-location": [ "error", "property" ],
+		"no-eq-null": "off",
+		"eqeqeq": [ "error", "always", { null: "ignore" } ],
+		"jest/no-large-snapshots": [ "warn", { maxSize: 50 } ],
+	},
+	// https://github.com/yannickcr/eslint-plugin-react#configuration
+	settings: {
+		// react: {
+		// 	version: "detect"
+		// }
+	}
+};
